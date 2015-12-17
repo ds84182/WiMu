131c97e2771d0e0e300ecce5b8749e511eaecf02
Dwayne Slater <ds84182@gmail.com>
2015-12-17 10:35:16 -0500

    Move font initialization with data include to it's C++ file
    Modules do not have access to the data headers right now, so including
    any file that includes Font.h would make the compiler error.

--- src/render/Font.cpp
+++ src/render/Font.cpp
@@ -1,6 +1,15 @@
 #include "Font.h"
 
+#include "Roboto_Regular_ttf.h"
+
 namespace Roboto {
 	FreeTypeGX *Body;
 	FreeTypeGX *Title;
+}
+
+namespace Font {
+	void init() {
+		Roboto::Body = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, FONTSIZE(14));
+		Roboto::Title = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, FONTSIZE(20));
+	}
 }
--- src/render/Font.h
+++ src/render/Font.h
@@ -7,13 +7,8 @@ namespace Roboto {
 	extern FreeTypeGX *Title;
 };
 
-#include "Roboto_Regular_ttf.h"
-
 #define FONTSIZE(x) (x*2)
 
 namespace Font {
-	inline void init() {
-		Roboto::Body = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, FONTSIZE(14));
-		Roboto::Title = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, FONTSIZE(20));
-	}
+	void init();
 };
