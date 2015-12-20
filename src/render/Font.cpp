f11d52658872b305c4b1cb5add3bcef8301ece10
Dwayne Slater <ds84182@gmail.com>
2015-12-19 21:14:46 -0500

    Font: Export Font sizes (in pixels) with font instances.

--- src/render/Font.cpp
+++ src/render/Font.cpp
@@ -3,13 +3,16 @@
 #include "Roboto_Regular_ttf.h"
 
 namespace Roboto {
+	int BodySize = FONTSIZE(14);
 	FreeTypeGX *Body;
+
+	int TitleSize = FONTSIZE(20);
 	FreeTypeGX *Title;
 }
 
 namespace Font {
 	void init() {
-		Roboto::Body = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, FONTSIZE(14));
-		Roboto::Title = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, FONTSIZE(20));
+		Roboto::Body = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, Roboto::BodySize);
+		Roboto::Title = new FreeTypeGX(Roboto_Regular_ttf, Roboto_Regular_ttf_size, Roboto::TitleSize);
 	}
 }
--- src/render/Font.h
+++ src/render/Font.h
@@ -2,12 +2,15 @@
 
 #include <util/FreeTypeGX.h>
 
+#define FONTSIZE(x) (x*2)
+
 namespace Roboto {
+	extern int BodySize;
 	extern FreeTypeGX *Body;
+
+	extern int TitleSize;
 	extern FreeTypeGX *Title;
 };
-
-#define FONTSIZE(x) (x*2)
 
 namespace Font {
 	void init();
